d45056c640ceb52fab98256c1e2ccdf80ecd7b6f
Ante Dragoje <antekrtalic2@gmail.com>
2021-11-13 19:03:51 +0100

    Created a Schedule class for handling multiple tasks

--- src/todoapp/Schedule.java
+++ src/todoapp/Schedule.java
@@ -1,4 +1,25 @@
 package todoapp;
 
+
+import java.util.HashMap;
+
 public class Schedule {
+    private HashMap<String, ToDoApplication> toDoThings;
+
+    public Schedule() {
+        toDoThings = new HashMap<String, ToDoApplication>();
+    }
+
+    public void addTask(String id, ToDoApplication toDoTask) {
+        toDoThings.put(id, toDoTask);
+    }
+
+    public void removeTask(String id) {
+        for (String task : this.toDoThings.keySet()) {
+            if (task.equals(id)) {
+                this.toDoThings.remove(id);
+                return;
+            }
+        }
+    }
 }
