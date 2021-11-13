58db7b9570406fe3e4260d8f6c78c4063168fae7
Ante Dragoje <antekrtalic2@gmail.com>
2021-11-13 18:32:57 +0100

    First commit

---
+++ src/java/todoapp/ToDoApplication.java
@@ -0,0 +1,13 @@
+package java.todoapp;
+
+public class ToDoApplication {
+    private String id;
+    private String task;
+
+    public ToDoApplication(String id, String task) {
+        this.id = id;
+        this.task = task;
+    }
+
+
+}
