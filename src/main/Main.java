7788e8b8baca179d5126fba51bfc41c1384c75d3
Ante Dragoje <antekrtalic2@gmail.com>
2021-11-14 09:41:30 +0100

    Trying to fix problems with inputs

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,24 +14,44 @@ public class Main {
         System.out.println("3. Delete");
         System.out.println("4. Exit");
 
-        String task = scanner.next();
+        String task = scanner.nextLine();
 
         Schedule scheduleList = new Schedule();
         int taskCounter = 1;
 
         do {
 
+
             if (task.equals("1")) {
                 System.out.println("Add your new task!");
-                String addTask = scanner.next();
+                String addTask = scanner.nextLine();
                 scheduleList.addTask(String.valueOf(taskCounter), new ToDoApplication(addTask));
             } else if (task.equals("2")) {
                 System.out.println("Pick up task you want to edit!");
-                String taskNuber = scanner.next();
+                String taskNumber = scanner.nextLine();
                 System.out.println("Choose new task to be replaced by old one: ");
-                String improvedTask = scanner.next();
-                scheduleList.updateTask(taskNuber, new ToDoApplication(improvedTask));
+                String improvedTask = scanner.nextLine();
+                scheduleList.updateTask(taskNumber, new ToDoApplication(improvedTask));
+            } else if (task.equals("3")) {
+                System.out.println("Choose task to remove");
+                String removeNumber = scanner.nextLine();
+                scheduleList.removeTask(removeNumber);
+            } else if (task.equals("4")) {
+                break;
+            } else {
+                System.out.println("Pick a number from the list!");
             }
+
+            scheduleList.printTasks();
+            System.out.println("");
+            System.out.println("What do you want to do?");
+            System.out.println("1. Add");
+            System.out.println("2. Update");
+            System.out.println("3. Delete");
+            System.out.println("4. Exit");
+
+            task = scanner.next();
+            taskCounter++;
 
         } while(!task.equals("4"));
     }
